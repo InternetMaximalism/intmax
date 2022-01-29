72120226e6c2b4005ad63e2aab5d360e2a3c344a
serinuntius <wasab1gj@gmail.com>
2022-01-29 16:30:35 +0900

    [wip] impl send_transaction

--- primitives/rpc/src/lib.rs
+++ primitives/rpc/src/lib.rs
@@ -1,19 +1,29 @@
-use ethereum_types::H256;
+use ethereum_types::{H256};
 use fc_rpc_core::types::TransactionRequest;
-use jsonrpc_core::{BoxFuture, Result};
+use jsonrpc_core::{BoxFuture, Result, futures::future};
 
 use intmax_json_rpc_api::EthApi as EthApiT;
+use tx_receiver::{TxReceiver, TxReceiverTrait};
 
-pub struct EthApi;
+pub struct EthApi {
+    tx_receiver: TxReceiver,
+}
 
 impl EthApi {
-    pub fn new() -> EthApi {
-        EthApi {}
+    pub fn new(tx_receiver: TxReceiver) -> EthApi {
+        EthApi {
+            tx_receiver
+        }
     }
 }
 
 impl EthApiT for EthApi {
-    fn send_transaction(&self, _: TransactionRequest) -> BoxFuture<Result<H256>> {
+    fn send_transaction(&self, req: TransactionRequest) -> BoxFuture<Result<H256>> {
+        match self.tx_receiver.validate_tx(&req) {
+            Ok(()) => (),
+            Err(e) => return Box::pin(future::err(e)),
+        }
+
         Box::pin(async move { Ok(H256::zero()) })
     }
 }
@@ -22,9 +32,12 @@ impl EthApiT for EthApi {
 mod tests {
     use super::*;
     use fc_rpc_core::types::TransactionRequest;
-    #[test]
-    fn it_works() {
-        let eth_api = EthApi::new();
-        eth_api.send_transaction(TransactionRequest::default());
+
+    #[tokio::test]
+    async fn it_works() {
+        let tx_receiver = TxReceiver::new();
+        let eth_api = EthApi::new(tx_receiver);
+
+        let _res = eth_api.send_transaction(TransactionRequest::default());
     }
 }
