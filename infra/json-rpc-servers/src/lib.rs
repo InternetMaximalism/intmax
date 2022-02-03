50c13017e3120ad04c40d68a7c5ddade387db72b
serinuntius <wasab1gj@gmail.com>
2022-02-04 00:01:55 +0900

    impl server start testing

--- infra/json-rpc-servers/src/lib.rs
+++ infra/json-rpc-servers/src/lib.rs
@@ -45,9 +45,39 @@ pub fn start_http_server(
 
 #[cfg(test)]
 mod tests {
+    use crate::middleware::{TracingMiddleware};
+    use crate::{rpc_handler, start_http_server};
+    use jsonrpc_core::{MetaIoHandler};
+    use reqwest;
+    use reqwest::StatusCode;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn success_server_start() {
+        let io = MetaIoHandler::with_middleware(TracingMiddleware::default());
+
+        let rpc_handler = rpc_handler(io);
+        let http_server = start_http_server(
+            &std::net::SocketAddr::new("127.0.0.1".parse().expect("set valid ip address."), 8080),
+            rpc_handler,
+        );
+
+        assert!(http_server.is_ok());
+
+        let client_fut = async move {
+            let c = reqwest::Client::new();
+            let res = c
+                .post("http://127.0.0.1:8080")
+                .json(&serde_json::json!({
+                    "jsonrpc": "2.0",
+                    "method": "rpc_methods",
+                    "id": 1
+                }))
+                .send()
+                .await
+                .expect("should success request");
+            assert_eq!(res.status(), StatusCode::OK);
+        };
+
+        tokio::runtime::Runtime::new().unwrap().block_on(client_fut);
     }
 }
