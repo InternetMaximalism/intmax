09f2509912a6a641a5da33f9760daa72fc172b57
serinuntius <wasab1gj@gmail.com>
2022-02-03 21:41:26 +0900

    ignore coverage

--- infra/json-rpc-api/src/lib.rs
+++ infra/json-rpc-api/src/lib.rs
@@ -3,6 +3,7 @@ use fc_rpc_core::types::TransactionRequest;
 use jsonrpc_core::{BoxFuture, Result};
 use jsonrpc_derive::rpc;
 
+// grcov: ignore-start
 #[rpc(server)]
 pub trait EthApi {
     /// Sends transaction; will block waiting for signer to return the
@@ -10,12 +11,4 @@ pub trait EthApi {
     #[rpc(name = "eth_sendTransaction")]
     fn send_transaction(&self, _: TransactionRequest) -> BoxFuture<Result<H256>>;
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+// grcov: ignore-end
