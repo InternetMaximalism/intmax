e31f5cba16af3dc4123cd706be8d702adee048a6
serinuntius <wasab1gj@gmail.com>
2022-02-07 15:13:26 +0900

    create dir if there is no contract dir

--- infra/eth-provider/src/bin/generate.rs
+++ infra/eth-provider/src/bin/generate.rs
@@ -19,6 +19,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let artifacts_dir = Path::new(root.as_path()).join(ARTIFACTS_PATH);
     let contracts_dir = Path::new(root.as_path()).join(CONTRACTS_DEST_PATH);
 
+    if !contracts_dir.is_dir() {
+        std::fs::create_dir(&contracts_dir).unwrap_or_else(|_| panic!("failed to create_dir {:?}", &contracts_dir));
+    }
+
     println!("loading artifacts ...");
 
     let artifacts = HardHatLoader::new()
