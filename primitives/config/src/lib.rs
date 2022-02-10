ca1c471626388b10fa153e671ccd5489cc6c477a
serinuntius <wasab1gj@gmail.com>
2022-02-10 09:55:52 +0900

    impl eth_config

--- primitives/config/src/lib.rs
+++ primitives/config/src/lib.rs
@@ -1,5 +1,6 @@
 use config::{Config as ConfigRs, ConfigError, File};
 use serde_derive::Deserialize;
+use std::fmt::{Display, Formatter};
 
 #[derive(Debug, Deserialize)]
 pub struct RpcServerConfig {
@@ -7,10 +8,58 @@ pub struct RpcServerConfig {
     pub ip: String,
 }
 
+#[derive(Debug, Deserialize, PartialEq)]
+pub enum Scheme {
+    Http,
+    Https,
+    Ws,
+    Wss,
+}
+
+impl Default for Scheme {
+    fn default() -> Self {
+        Self::Http
+    }
+}
+
+impl Display for Scheme {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Scheme::Http => write!(f, "http"),
+            Scheme::Https => write!(f, "https"),
+            Scheme::Ws => write!(f, "ws"),
+            Scheme::Wss => write!(f, "wss"),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Default)]
+pub struct EthConfig {
+    pub committer_key: String,
+    pub port: u16,
+    pub host: String,
+    pub scheme: Scheme,
+}
+
+impl EthConfig {
+    pub fn node_url(&self) -> String {
+        format!("{}://{}:{}", self.scheme, self.host, self.port)
+    }
+
+    pub fn is_http(&self) -> bool {
+        self.scheme == Scheme::Http || self.scheme == Scheme::Https
+    }
+
+    pub fn is_ws(&self) -> bool {
+        self.scheme == Scheme::Ws || self.scheme == Scheme::Wss
+    }
+}
+
 #[derive(Debug, Deserialize)]
 pub struct Config {
     pub http_server: RpcServerConfig,
     pub ws_server: RpcServerConfig,
+    pub eth_server: EthConfig,
 }
 
 pub enum ConfigKind {
@@ -52,5 +101,28 @@ mod tests {
         assert_eq!(config.http_server.ip, "127.0.0.1");
         assert_eq!(config.ws_server.port, 3030);
         assert_eq!(config.ws_server.ip, "127.0.0.1");
+        assert_eq!(
+            config.eth_server.committer_key,
+            "10d18ee85b1a2e1d4b47feed91074a6bb4a17b55005144338208a0be031752d3"
+        );
+        assert_eq!(config.eth_server.port, 8545);
+        assert_eq!(config.eth_server.host, "127.0.0.1");
+        assert_eq!(config.eth_server.scheme, Scheme::Http);
+        assert_eq!(config.eth_server.node_url(), "http://127.0.0.1:8545");
+        assert!(config.eth_server.is_http());
+        assert!(!config.eth_server.is_ws());
+
+        assert_eq!(format!("{}", Scheme::Http), "http");
+        assert_eq!(format!("{}", Scheme::Https), "https");
+        assert_eq!(format!("{}", Scheme::Ws), "ws");
+        assert_eq!(format!("{}", Scheme::Wss), "wss");
+
+        let config = EthConfig {
+            scheme: Scheme::Ws,
+            ..EthConfig::default()
+        };
+
+        assert!(config.is_ws());
+        assert!(!config.is_http());
     }
 }
