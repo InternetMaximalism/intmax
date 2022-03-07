8d78d5d15f0bf32b2095eab8c80e4fedceef2462
Kazuki Kasahara <nuderebatsin.roal@gmail.com>
2022-03-07 16:54:45 +0900

    feat: implement base of verkle

--- primitives/verkle/src/lib.rs
+++ primitives/verkle/src/lib.rs
@@ -1,27 +1,113 @@
-use codec::{Decode, Encode};
+use codec::Encode;
+use franklin_crypto::bellman::bn256::G1Affine;
+use franklin_crypto::bellman::bn256::Fr;
+use verkle_tree::bn256_verkle_tree::proof::VerkleProof;
+use verkle_tree::bn256_verkle_tree::leaf::LeafNodeWith32BytesValue;
+use verkle_tree::bn256_verkle_tree::VerkleTreeWith32BytesKeyValue;
+use verkle_tree::ipa_fr::config::IpaConfig;
+use bytes::BufMut;
 
 #[derive(Debug)]
 pub enum StorageError {
     InvalidError,
 }
 
-pub struct Proof {
-    pub siblings: Vec<Vec<u8>>,
-    pub leaf: Vec<u8>,
-}
-
 pub type Result<T> = std::result::Result<T, StorageError>;
 
+pub type Proof = VerkleProof<[u8; 32], LeafNodeWith32BytesValue<G1Affine>, G1Affine, IpaConfig<G1Affine>>;
+
+pub type Elements = verkle_tree::verkle_tree::witness::Elements<Fr>;
+
+#[derive(Debug)]
+pub struct Storage {
+    tree: VerkleTreeWith32BytesKeyValue,
+    committer:  IpaConfig<G1Affine>,
+}
+
 pub trait VerkleStorage {
-    fn inclusion_proof<K: Clone + AsRef<[u8]>>(&self, key: &K) -> Result<Proof>;
-    fn verify_proof(&self, proof: &Proof) -> Result<()>;
-    fn put<K: Clone + AsRef<[u8]>, V: Encode>(&self, key: &K, data: &V) -> Result<()>;
-    fn remove<K: Clone + AsRef<[u8]>, V: Encode>(&self, key: &K, data: &V) -> Result<()>;
-    fn get<K: Clone + AsRef<[u8]>, V: Decode>(&self, key: &K) -> Option<V>;
+    fn new() -> Self;
+    fn put<K: Clone + AsRef<[u8]>, V: Encode>(&mut self, key: &K, data: &V) -> Result<()>;
+    fn get<K: Clone + AsRef<[u8]>>(&self, key: &K) -> Result<std::option::Option<&[u8; 32]>>;
+    fn remove<K: Clone + AsRef<[u8]>>(&mut self, key: &K) -> Result<()>;
+    fn inclusion_proof<K: Clone + AsRef<[u8]>>(&mut self, key: &K) -> Result<(Proof, Elements)>;
+    fn verify_proof(&mut self, proof: Proof, elements: Elements) -> Result<()>;
 }
 
 pub trait VerkleSMTStorage: VerkleStorage {
     fn inclusion_empty_proof<K: Clone + AsRef<[u8]>>(&self, key: &K) -> Result<Proof>;
+}
+
+fn generate_byte_key<K: Clone + AsRef<[u8]>>(key: &K) -> [u8; 32] {
+    let raw = key.as_ref();
+    let raw_u8: &[u8] = &raw[..];
+    let mut byte_key = [0; 32];
+    let mut buf = &mut byte_key[..];
+    buf.put(&raw_u8[..]);
+
+    byte_key
+}
+
+fn generate_byte_value<V: Encode>(value: &V) -> [u8; 32] {
+    let raw: &[u8] = &value.encode();
+    let mut byte_value = [0; 32];
+    let mut buf = &mut byte_value[..];
+    buf.put(&raw[..]);
+
+    byte_value
+}
+
+impl VerkleStorage for Storage {
+    fn new() -> Self {
+        let domain_size = 256;
+        let committer = IpaConfig::new(domain_size);
+        let tree: VerkleTreeWith32BytesKeyValue = VerkleTreeWith32BytesKeyValue::new(committer.clone());
+
+        Self { tree, committer }
+    }
+
+    fn put<K: Clone + AsRef<[u8]>, V: Encode>(&mut self, key: &K, data: &V) -> Result<()> {
+        let byte_key = generate_byte_key(key);
+        let byte_value = generate_byte_value(data);
+
+        VerkleTreeWith32BytesKeyValue::insert(&mut self.tree, byte_key, byte_value);
+
+        Ok(())
+    }
+
+    fn get<K: Clone + AsRef<[u8]>>(&self, _key: &K) -> Result<std::option::Option<&[u8; 32]>>  {
+        let byte_key = generate_byte_key(_key);
+        let stored_value: Option<&[u8; 32]> = VerkleTreeWith32BytesKeyValue::get(&self.tree, &byte_key);
+
+        Ok(stored_value)
+    }
+
+    fn remove<K: Clone + AsRef<[u8]>>(&mut self, _key: &K) -> Result<()> {
+        let byte_key = generate_byte_key(_key);
+
+        VerkleTreeWith32BytesKeyValue::remove(&mut self.tree, &byte_key);
+
+        Ok(())
+    }
+
+    fn inclusion_proof<K: Clone + AsRef<[u8]>>(&mut self, _key: &K) -> Result<(Proof, Elements)> {
+        let byte_key = generate_byte_key(_key);
+
+        let keys = [byte_key];
+        let (proof, elements) = VerkleProof::create(&mut self.tree, &keys).unwrap();
+
+        Ok((proof, elements))
+    }
+
+    fn verify_proof(&mut self, proof: Proof, elements: Elements) -> Result<()> {
+        let is_valid: bool =
+        VerkleProof::check(&proof, &elements.zs, &elements.ys, &self.committer).unwrap();
+        println!("is_valid: {:?}", is_valid);
+
+        match VerkleProof::check(&proof, &elements.zs, &elements.ys, &self.committer).unwrap() {
+            true => Ok(()),
+            false => Err(StorageError::InvalidError),
+        }
+    }
 }
 
 #[cfg(test)]
