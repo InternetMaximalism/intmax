f3d4175f54b88499d545cfb691a38c7bcb2272e6
Kazuki Kasahara <nuderebatsin.roal@gmail.com>
2022-03-10 15:42:01 +0900

    feat: test verkle base

--- primitives/verkle/src/lib.rs
+++ primitives/verkle/src/lib.rs
@@ -1,11 +1,11 @@
+use bytes::BufMut;
 use codec::Encode;
-use franklin_crypto::bellman::bn256::G1Affine;
 use franklin_crypto::bellman::bn256::Fr;
-use verkle_tree::bn256_verkle_tree::proof::VerkleProof;
+use franklin_crypto::bellman::bn256::G1Affine;
 use verkle_tree::bn256_verkle_tree::leaf::LeafNodeWith32BytesValue;
+use verkle_tree::bn256_verkle_tree::proof::VerkleProof;
 use verkle_tree::bn256_verkle_tree::VerkleTreeWith32BytesKeyValue;
 use verkle_tree::ipa_fr::config::IpaConfig;
-use bytes::BufMut;
 
 #[derive(Debug)]
 pub enum StorageError {
@@ -14,14 +14,15 @@ pub enum StorageError {
 
 pub type Result<T> = std::result::Result<T, StorageError>;
 
-pub type Proof = VerkleProof<[u8; 32], LeafNodeWith32BytesValue<G1Affine>, G1Affine, IpaConfig<G1Affine>>;
+pub type Proof =
+    VerkleProof<[u8; 32], LeafNodeWith32BytesValue<G1Affine>, G1Affine, IpaConfig<G1Affine>>;
 
 pub type Elements = verkle_tree::verkle_tree::witness::Elements<Fr>;
 
 #[derive(Debug)]
 pub struct Storage {
     tree: VerkleTreeWith32BytesKeyValue,
-    committer:  IpaConfig<G1Affine>,
+    committer: IpaConfig<G1Affine>,
 }
 
 pub trait VerkleStorage {
@@ -60,7 +61,8 @@ impl VerkleStorage for Storage {
     fn new() -> Self {
         let domain_size = 256;
         let committer = IpaConfig::new(domain_size);
-        let tree: VerkleTreeWith32BytesKeyValue = VerkleTreeWith32BytesKeyValue::new(committer.clone());
+        let tree: VerkleTreeWith32BytesKeyValue =
+            VerkleTreeWith32BytesKeyValue::new(committer.clone());
 
         Self { tree, committer }
     }
@@ -74,9 +76,10 @@ impl VerkleStorage for Storage {
         Ok(())
     }
 
-    fn get<K: Clone + AsRef<[u8]>>(&self, _key: &K) -> Result<std::option::Option<&[u8; 32]>>  {
+    fn get<K: Clone + AsRef<[u8]>>(&self, _key: &K) -> Result<std::option::Option<&[u8; 32]>> {
         let byte_key = generate_byte_key(_key);
-        let stored_value: Option<&[u8; 32]> = VerkleTreeWith32BytesKeyValue::get(&self.tree, &byte_key);
+        let stored_value: Option<&[u8; 32]> =
+            VerkleTreeWith32BytesKeyValue::get(&self.tree, &byte_key);
 
         Ok(stored_value)
     }
@@ -100,7 +103,7 @@ impl VerkleStorage for Storage {
 
     fn verify_proof(&mut self, proof: Proof, elements: Elements) -> Result<()> {
         let is_valid: bool =
-        VerkleProof::check(&proof, &elements.zs, &elements.ys, &self.committer).unwrap();
+            VerkleProof::check(&proof, &elements.zs, &elements.ys, &self.committer).unwrap();
         println!("is_valid: {:?}", is_valid);
 
         match VerkleProof::check(&proof, &elements.zs, &elements.ys, &self.committer).unwrap() {
@@ -113,50 +116,99 @@ impl VerkleStorage for Storage {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use byteorder::{ByteOrder, LittleEndian};
+    use codec::Decode;
 
-    struct MockVeckleStorage;
-
-    impl VerkleStorage for MockVeckleStorage {
-        fn inclusion_proof<K: Clone + AsRef<[u8]>>(&self, _key: &K) -> Result<Proof> {
-            Ok(Proof {
-                siblings: vec![],
-                leaf: vec![],
-            })
-        }
-        fn verify_proof(&self, _proof: &Proof) -> Result<()> {
-            Ok(())
-        }
-        fn put<K: Clone + AsRef<[u8]>, V: Encode>(&self, _key: &K, _data: &V) -> Result<()> {
-            Ok(())
-        }
-        fn remove<K: Clone + AsRef<[u8]>, V: Encode>(&self, _key: &K, _data: &V) -> Result<()> {
-            Ok(())
-        }
-        fn get<K: Clone + AsRef<[u8]>, V: Decode>(&self, _key: &K) -> Option<V> {
-            None
-        }
+    #[derive(Encode, Decode, PartialEq, Debug)]
+    struct Value {
+        pub a: u32,
     }
 
-    impl VerkleSMTStorage for MockVeckleStorage {
-        fn inclusion_empty_proof<K: Clone + AsRef<[u8]>>(&self, _key: &K) -> Result<Proof> {
-            Ok(Proof {
-                siblings: vec![],
-                leaf: vec![],
-            })
+    fn get_index(v: Vec<u32>) -> usize {
+        for (i, &item) in v.iter().enumerate() {
+            if item == 0 {
+                return i;
+            }
         }
+
+        return v.len();
+    }
+
+    fn get_value(v: &[u8; 32], index: usize) -> Value {
+        let mut raw: &[u8] = &v[0..index + 3];
+
+        Value::decode(&mut raw).unwrap()
+    }
+
+    fn get_step_vec(v: &[u8; 32]) -> Vec<u32> {
+        let mut result: Vec<u32> = Vec::new();
+        for i in (0..v.len()).step_by(4) {
+            result.push(LittleEndian::read_u32(&v[i..]));
+        }
+
+        result
+    }
+
+    fn get_decoded(v: &[u8; 32]) -> Value {
+        let vec = get_step_vec(v);
+        let index = get_index(vec);
+        let decoded = get_value(&v, index);
+
+        decoded
     }
 
     #[test]
-    fn wrong_test() {
-        let storage = MockVeckleStorage {};
+    fn works_put() {
+        let mut storage: Storage = VerkleStorage::new();
 
         let k = vec![5, 10];
         let v: i32 = 10;
+        let res = storage.put(&k, &v).expect("put ok.");
+        assert_eq!(res, ());
+    }
+
+    #[test]
+    fn works_get() {
+        let mut storage: Storage = VerkleStorage::new();
+
+        let k = vec![5, 10];
+        let v = Value { a: 10 };
+        storage.put(&k, &v).expect("put ok.");
+        let res = storage.get(&k).expect("get ok.").unwrap();
+
+        let decoded = get_decoded(res);
+
+        assert_eq!(v, decoded);
+    }
+
+    #[test]
+    fn works_remove() {
+        let mut storage: Storage = VerkleStorage::new();
+
+        let k = vec![5, 10];
+        let v = Value { a: 10 };
+
+        storage.put(&k, &v).expect("put ok.");
+        let res = storage.get(&k).expect("get ok.");
+        assert!(res.is_some());
+
+        storage.remove(&k).expect("remove ok.");
+        let res = storage.get(&k).expect("get ok.");
+        assert!(res.is_none());
+    }
+
+    #[test]
+    fn works_verify() {
+        let mut storage: Storage = VerkleStorage::new();
+
+        let k = vec![5, 10];
+        let v = Value { a: 10 };
         storage.put(&k, &v).expect("put ok.");
 
-        let proof = storage
-            .inclusion_proof(&k)
-            .expect("failed inclusion proof.");
-        storage.verify_proof(&proof).expect("failed verify proof");
+        let (proof, elements) = storage.inclusion_proof(&k).expect("inclusion proof ok.");
+        let res = storage
+            .verify_proof(proof, elements)
+            .expect("verify proof ok");
+        assert_eq!(res, ());
     }
 }
